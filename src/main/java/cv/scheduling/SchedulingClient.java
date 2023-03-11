4db1e7150864c718d47404d75a32fbd3fd605555
Mendy2050 <876446372@qq.com>
2023-03-11 20:42:23 +0000

    upload on March 11 2023 for the second time

--- src/main/java/cv/scheduling/SchedulingClient.java
+++ src/main/java/cv/scheduling/SchedulingClient.java
@@ -1,13 +1,13 @@
-
 package cv.scheduling;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import cv.scheduling.SchedulingServiceGrpc;
 import cv.scheduling.SchedulingServiceGrpc.SchedulingServiceBlockingStub;
 import cv.scheduling.SchedulingServiceGrpc.SchedulingServiceStub;
 
-
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 public class SchedulingClient {
@@ -44,72 +44,129 @@ public class SchedulingClient {
 
 	
 	
-	//server streaming rpc
-	public void streamResources(String course, String teacher, String equipment) {
-		SchedulingRequest request = SchedulingRequest.newBuilder()
-									.setCourseId(course)
-									.setTeacherId(teacher)
-									.setEquipment(1,equipment)
-									.build();
 
-		stub.streamResources(request, new StreamObserver<ResourceResponse>() {
-				@Override
-				public void onNext(ResourceResponse response) {
-					System.out.println("Received response: " + response.getRoom() + ", " + response.getTeacher() + ", " + response.getEquipment());
-				}
-				
-				@Override
-				public void onError(Throwable t) {
-					System.err.println("Encountered error: " + t);
-				}
-				
-				@Override
-				public void onCompleted() {
-					System.out.println("Request completed");
-				}
-		});
+	
+	
+	// blocking server-streaming
+	public static void checkAvailabilityBlocking(SchedulingRequest request) { 
+		try {
+			Iterator<ResourceAvailabilityResponse> responses = blockingStub.checkAvailability(request);
+
+			while (responses.hasNext()) {
+				ResourceAvailabilityResponse temp = responses.next();
+				System.out.println("resource_id: " + temp.getResourceId() + " available_times: " + temp.getAvailableTimesList());
+			}
+
+		} catch (StatusRuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// asynchronous server-streaming
+	public static void checkAvailabilityAsync(SchedulingRequest request) {
+	StreamObserver<ResourceAvailabilityResponse> responseObserver = new StreamObserver<ResourceAvailabilityResponse>() {
+
+	    @Override
+	    public void onNext(ResourceAvailabilityResponse value) {
+	        System.out.println("received resource_id: " + value.getResourceId() + " available_times: " + value.getAvailableTimesList());
+	    }
+
+	    @Override
+	    public void onError(Throwable t) {
+	        t.printStackTrace();
+
+	    }
+
+	    @Override
+	    public void onCompleted() {
+	        System.out.println("stream is completed ...");
+	    }
+
+	};
+
+	asyncStub.checkAvailability(request, responseObserver);
+
+	try {
+	    Thread.sleep(15000);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
 	}
 	
 	
 	
-	public void batchSchedule() {
-		StreamObserver<SchedulingRequest> requestObserver = stub.batchSchedule(new StreamObserver<ScheduleResponse>() {
-			@Override
-			public void onNext(ScheduleResponse response) {
-				System.out.println("Received response: " + response.getMessage());
-			}
-			
-			@Override
-			public void onError(Throwable t) {
-				System.err.println("Encountered error: " + t);
-			}
+	
+	
+	// client-streaming
+	public static void batchSchedule() {
+	    StreamObserver<SchedulingResultResponse> responseObserver = new StreamObserver<SchedulingResultResponse>() {
 
-			@Override
-			public void onCompleted() {
-				System.out.println("Request completed");
-			}
-		});
-		
-		requestObserver.onNext(SchedulingRequest.newBuilder()
-						.setCourseId("Course 1")
-						.setTeacherId("Teacher 1")
-						.setEquipment(1,"Equipment 1")
-						.build());
-		
-		requestObserver.onNext(SchedulingRequest.newBuilder()
-						.setCourseId("Course 2")
-						.setTeacherId("Teacher 2")
-						.setEquipment(2,"Equipment 2")
-						.build());
+	        @Override
+	        public void onNext(SchedulingResultResponse msg) {
+	            System.out.println("Batch scheduling result received: " + msg.getMessage());
+	        }
 
-		requestObserver.onNext(SchedulingRequest.newBuilder()
-						.setCourseId("Course 3")
-						.setTeacherId("Teacher 3")
-						.setEquipment(3,"Equipment 3")
-						.build());
+	        @Override
+	        public void onError(Throwable t) {
+	            t.printStackTrace();
+	        }
 
-		requestObserver.onCompleted();
+	        @Override
+	        public void onCompleted() {
+	            System.out.println("Batch scheduling completed.");
+	        }
+
+	    };
+
+	    StreamObserver<SchedulingRequest> requestObserver = asyncStub.batchSchedule(responseObserver);
+	    try {
+	        requestObserver.onNext(SchedulingRequest.newBuilder()
+	            .setCourseId("course1")
+	            .setTeacherId("teacher1")
+	            .addEquipment("projector")
+	            .addEquipment("whiteboard")
+	            .setNumStudents(30)
+	            .setStartTime(1647026400000L)
+	            .setEndTime(1647030000000L)
+	            .build());
+	        Thread.sleep(500);
+
+	        requestObserver.onNext(SchedulingRequest.newBuilder()
+	            .setCourseId("course2")
+	            .setTeacherId("teacher2")
+	            .addEquipment("computer")
+	            .setNumStudents(25)
+	            .setStartTime(1647040800000L)
+	            .setEndTime(1647044400000L)
+	            .build());
+	        Thread.sleep(500);
+
+	        requestObserver.onNext(SchedulingRequest.newBuilder()
+	            .setCourseId("course3")
+	            .setTeacherId("teacher3")
+	            .addEquipment("projector")
+	            .addEquipment("whiteboard")
+	            .addEquipment("microphone")
+	            .setNumStudents(40)
+	            .setStartTime(1647055200000L)
+	            .setEndTime(1647058800000L)
+	            .build());
+	        Thread.sleep(500);
+
+	        // Mark the end of requests
+	        requestObserver.onCompleted();
+
+	        Thread.sleep(10000);
+
+	    } catch (RuntimeException e) {
+	        e.printStackTrace();
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
 	}
+
+	
+	
 
 	
 	
